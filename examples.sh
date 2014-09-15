170616e27a4babb1871d6bbb22e19c14a689ab0b
David Simic <dpsimic@gmail.com>
2014-09-15 18:20:03 -0400

    examples

--- examples.sh
+++ examples.sh
@@ -1,6 +1,9 @@
+OUTROOT=./test/tmp/
+# clean OUTROOT
+rm -rf $OUTROOT
 
-python docsplit.py test/a.pdf --outdir ./test/tmp/a/ 
+python docsplit.py test/a.pdf --outdir $OUTROOT/a/ 
 
-python docsplit.py test/b.doc --outdir ./test/tmp/b/ 
+python docsplit.py test/b.doc --outdir $OUTROOT/b/ 
 
-python docsplit.py test/c.docx --outdir ./test/tmp/c/ 
+python docsplit.py test/c.docx --outdir $OUTROOT/c/ 
